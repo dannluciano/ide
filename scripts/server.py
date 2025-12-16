b6d0a00fd7e84f81a57b783e99923f9fee7b2309
Dann Luciano <dannluciano@gmail.com>
2025-12-16 15:09:25 -0300

    Fix Pixi Build

---
+++ scripts/server.py
@@ -0,0 +1,12 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+
+class Handler(SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header("Cross-Origin-Opener-Policy", "same-origin")
+        self.send_header("Cross-Origin-Embedder-Policy", "require-corp")
+        super().end_headers()
+
+
+HTTPServer(("localhost", 8000), Handler).serve_forever()
+print("Server running on: http://localhost:8000/")
